bd2202eee930ff8748fb412307fcf145f893ffeb
Nikhil Shukla <nikhilshukla8652@gmail.com>
2025-05-26 21:55:36 +0530

    update phoneModel

--- Backend/models/phoneModel.js
+++ Backend/models/phoneModel.js
@@ -1,11 +1,15 @@
 import mongoose from "mongoose";
 
 const phoneSchema = new mongoose.Schema({
-  name: String,
-  image: String,
-  description: String,
-  bestFor: String,
-  affiliateLink: String,
+ name: String,
+image: String,
+description: String,
+bestFor: String,
+pros: [String],
+cons: [String],
+verdict: String,
+affiliateLink: String,
+slug: String // for SEO-friendly URLs
 });
 
 export default mongoose.model("Phone", phoneSchema);
