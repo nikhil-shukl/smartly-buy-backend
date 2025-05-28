465dfeb772db4b78a9d857d6dc166a5910432885
Nikhil Shukla <nikhilshukla8652@gmail.com>
2025-05-28 16:55:30 +0530

    update homeModel

--- Backend/models/homeModel.js
+++ Backend/models/homeModel.js
@@ -3,45 +3,25 @@ import mongoose from 'mongoose';
 import slugify from 'slugify';
 
 const homeCardSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  slug: {
-    type: String,
-    unique: true,
-    lowercase: true,
-  },
-  image: {
-    type: String,
-    required: true,
-  },
-  content: {
-    type: String,
-    required: true,
-  },
-  verdict: {
-    type: String,
-    required: true,
-  },
-  isHero: {
-    type: Boolean,
-    default: false,
-  },
-  category: {
-    type: String,
-    enum: ['Smartphones', 'Laptops', 'Earbuds'],
-    required: true,
-  },
-  affiliateLink: {
-    type: String,
-    default: 'soon',
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
+   title: String,
+  description: String,
+  image: String,
+  slug: { type: String, unique: true },
+  content: String,
+  metaTitle: String,
+  metaDescription: String,
+  products: [
+    {
+      rank: Number,
+      name: String,
+      image: String,
+      pros: [String],
+      cons: [String],
+      verdict: String,
+      affiliateLink: String
+    }
+  ],
+  createdAt: { type: Date, default: Date.now }
 });
 
 homeCardSchema.pre('save', function (next) {
